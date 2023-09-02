ed4c1262cf86e65db109f8e81675ce5cc1e8ab21
kiranprasath <kiranprasath@kiranprasaths-MacBook-Pro.local>
2023-09-02 18:18:11 +0530

    Pushed succes

---
+++ lib/Home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  )),
+              actions: [
+                IconButton(
+                    onPressed: (() {}),
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    ))
+              ],
+              shadowColor: Colors.transparent,
+              backgroundColor: Colors.transparent,
+              centerTitle: true,
+              title: Text(
+                'Balbalan',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )),
+          body: Container(
+            height: 800,
+            width: 500,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Top League',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.all(8),
+                        children: <Widget>[
+                          Container(
+                            color: Colors.grey,
+                            child: const Center(child: Text('Entry A')),
+                          ),
+                          Container(
+                            color: Colors.grey,
+                            child: const Center(child: Text('Entry B')),
+                          ),
+                          Container(
+                            color: Colors.grey,
+                            child: const Center(child: Text('Entry C')),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:appps/Home.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(HomePage());
+}

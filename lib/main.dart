02c3fc2701e934e190431cf846b17d1076d9e6bc
DineshKumarS2003 <83019805+DineshKumarS2003@users.noreply.github.com>
2023-09-05 17:22:20 +0530

    New project changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
-import 'package:appps/Home.dart';
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(HomePage());
+// ignore: camel_case_types
+class cart extends StatefulWidget {
+  const cart({super.key});
+
+  @override
+  State<cart> createState() => _cartState();
+}
+
+// ignore: camel_case_types
+class _cartState extends State<cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+            const Text('Add Cart')
+          ],
+        ),
+      ],
+    );
+  }
 }
